784ee0cdf5b75ebe27d92399b3c7471a1f189dca
Elliott Demone <124749710+N1GHTM4R@users.noreply.github.com>
2024-12-17 21:40:28 -0500

    feat(main, soundplayer): add looping sound functionality with start and stop buttons

--- src/soundplayer.py
+++ src/soundplayer.py
@@ -1,5 +1,8 @@
 from playsound import playsound # type: ignore #NOTE: This just ignores the error here, its really annoying lol.
 import os
+import threading
+
+looping = False
 
 def play_sound(file_name):
     # * Get the absolute path to the sound file
@@ -16,3 +19,19 @@ def play_sound(file_name):
     else:
         # ! Error: Handle the case where the file does not exist
         print("File does not exist at the specified path.")
+        
+        
+def loopsound(file_name):
+    global looping
+    looping = True
+
+    def loop():
+        while looping:
+            play_sound(file_name)
+
+    # Start the loop in a separate thread
+    threading.Thread(target=loop).start()
+
+def stop_loopsound():
+    global looping
+    looping = False
